9c1754ac64b2a5530508e347023d33657cb35ca5
stevestring <stevestring@gmail.com>
2019-01-02 17:50:11 -0500

    Added google analytics, final https settings

--- src/settings.js
+++ src/settings.js
@@ -5,8 +5,12 @@ var settings =
         // "socketurl": "http://team-room-prod.us-east-1.elasticbeanstalk.com",
         // "serverurl": "http://localhost:3000",
         // "socketurl":"http://localhost:3000",
-        "serverurl": "http://io.team-room.com",
-        "socketurl": "http://io.team-room.com",        
+        // "serverurl": "http://io.team-room.com",
+        // "socketurl": "http://io.team-room.com",       
+        "serverurl": "https://io.team-room.com",
+        "socketurl": "https://io.team-room.com",  
+        // "serverurl": "http://team-room.com/api",
+        // "socketurl": "http://team-room.com/api",  
         "":""
     }
 
